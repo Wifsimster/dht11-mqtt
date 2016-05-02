9021a7b54b543ffb95cb27d50f6b8a9055d6e112
bot <contact@arca-computing.fr>
2016-05-02 14:37:42 +0200

    Add info on mqtt data

--- main.lua
+++ main.lua
@@ -1,8 +1,6 @@
 require('config')
 require('dht11')
 
-DATA_PIN = 3 -- GPIO_0
-
 -- MQTT client
 TOPIC = "/sensors/bureau/dht11/data"
 
@@ -19,18 +17,20 @@ tmr.alarm(2, 1000, 1, function()
         humidity = tonumber(dht11_data.humidity)
         
         -- First send data
-        DATA = '{"temperature":"'..temperature..'","humidity":"'..humidity..'"}'
+        DATA = '{"mac":"'..wifi.sta.getmac()..'", "ip":"'..wifi.sta.getip()..'",'
+        DATA = DATA..'"temperature":"'..temperature..'","humidity":"'..humidity..'"}'
         m:publish(TOPIC, DATA, 0, 0, function(conn)
             print(CLIENT_ID.." sending data: "..DATA.." to "..TOPIC)
         end)
         
         -- Check every 5s for values change
-        tmr.alarm(1, 5000, 1, function()
+        tmr.alarm(1, REFRESH_RATE, 1, function()
             dht11_data = getData(DATA_PIN)
             tmp_temperature = tonumber(dht11_data.temperature)
             tmp_humidity = tonumber(dht11_data.humidity)
-            if(temperature ~= tmp_temperature or humidity ~= tmp_humidity) then
-                DATA = '{"temperature":"'..tmp_temperature..'","humidity":"'..tmp_humidity..'"}'
+            if(temperature ~= tmp_temperature or humidity ~= tmp_humidity) then              
+                DATA = '{"mac":"'..wifi.sta.getmac()..'", "ip":"'..wifi.sta.getip()..'",'
+                DATA = DATA..'"temperature":"'..tmp_temperature..'","humidity":"'..tmp_humidity..'"}'
                 -- Publish a message (QoS = 0, retain = 0)
                 m:publish(TOPIC, DATA, 0, 0, function(conn)
                     print(CLIENT_ID.." sending data: "..DATA.." to "..TOPIC)
