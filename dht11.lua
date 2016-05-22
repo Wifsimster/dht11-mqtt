5845f8d7658557ccd5c4974d06a8f15fb2052ff8
bot <contact@arca-computing.fr>
2016-05-22 19:43:46 +0200

    Add ntp data

--- dht11.lua
+++ dht11.lua
@@ -5,52 +5,31 @@ function getData(pin)
   temperatureDec = 0
   checksum = 0
   checksumTest = 0
-
-  -- Data stream acquisition timing is critical. There's
-  -- barely enough speed to work with to make this happen.
-  -- Pre-allocate vars used in loop.
-
   bitStream = {}
   for j = 1, 40, 1 do
     bitStream[j] = 0
   end
   bitlength = 0
-
   gpio.mode(pin, gpio.OUTPUT)
   gpio.write(pin, gpio.LOW)
   tmr.delay(20000)
-  
-  -- Use Markus Gritsch trick to speed up read/write on GPIO
   gpio_read=gpio.read
   gpio_write=gpio.write
-
   gpio.mode(pin, gpio.INPUT)
-
-  -- Bus will always let up eventually, don't bother with timeout
   while (gpio_read(pin)==0) do end
-
   c=0
   while (gpio_read(pin)==1 and c<100) do c=c+1 end
-
-  -- Bus will always let up eventually, don't bother with timeout
   while (gpio_read(pin)==0) do end
-
   c=0
   while (gpio_read(pin)==1 and c<100) do c=c+1 end
-
-  -- Acquisition loop
   for j = 1, 40, 1 do
     while (gpio_read(pin)==1 and bitlength<10) do
       bitlength=bitlength+1
     end
     bitStream[j]=bitlength
     bitlength=0
-    
-    -- Bus will always let up eventually, don't bother with timeout
     while (gpio_read(pin)==0) do end
   end
-
-  -- DHT data acquired, process.
   for i = 1, 8, 1 do
     if (bitStream[i+0] > 2) then
       humidity = humidity+2^(8-i)
@@ -75,17 +54,10 @@ function getData(pin)
     if (bitStream[i+32] > 2) then
       checksum = checksum+2^(8-i)
     end
-  end
+  end  
   checksumTest=(humidity+humidityDec+temperature+temperatureDec) % 0xFF
-
-  print ("Temperature: "..temperature.."."..temperatureDec)
-  print ("Humidity: "..humidity.."."..humidityDec)
---  print ("ChecksumReceived: "..checksum)
---  print ("ChecksumTest: "..checksumTest)
-
   rst = {}
   rst["temperature"] = temperature.."."..temperatureDec
   rst["humidity"] = humidity.."."..humidityDec
-
   return rst
 end
--- main.lua
+++ main.lua
@@ -1,45 +1,23 @@
 require('config')
 require('dht11')
 
--- MQTT client
-TOPIC = "/sensors/bureau/dht11/data"
+TOPIC = "/sensors/"..LOCATION.."/dht11/data"
 
 -- Init client with keepalive timer 120sec
 m = mqtt.Client(CLIENT_ID, 120, "", "")
 
-tmr.alarm(2, 1000, 1, function()
-    tmr.stop(2)
-    print("Connecting to MQTT: "..BROKER_IP..":"..BROKER_PORT.."...")
-    m:connect(BROKER_IP, BROKER_PORT, 0, function(conn)
-        print("Connected to MQTT: "..BROKER_IP..":"..BROKER_PORT.." as "..CLIENT_ID)
+print("Connecting to MQTT: "..BROKER_IP..":"..BROKER_PORT.."...")
+m:connect(BROKER_IP, BROKER_PORT, 0, 1, function(conn)
+    print("Connected to MQTT: "..BROKER_IP..":"..BROKER_PORT.." as "..CLIENT_ID)
+    tmr.alarm(1, REFRESH_RATE, 1, function()
         dht11_data = getData(DATA_PIN)
         temperature = tonumber(dht11_data.temperature)
-        humidity = tonumber(dht11_data.humidity)
-        
-        -- First send data
+        humidity = tonumber(dht11_data.humidity)        
         DATA = '{"mac":"'..wifi.sta.getmac()..'", "ip":"'..wifi.sta.getip()..'",'
-        DATA = DATA..'"temperature":"'..temperature..'","humidity":"'..humidity..'"}'
+        DATA = DATA..'"temperature":"'..temperature..'","humidity":"'..humidity..'"}'        
+        -- Publish a message (QoS = 0, retain = 0)
         m:publish(TOPIC, DATA, 0, 0, function(conn)
             print(CLIENT_ID.." sending data: "..DATA.." to "..TOPIC)
         end)
-        
-        -- Check every 5s for values change
-        tmr.alarm(1, REFRESH_RATE, 1, function()
-            dht11_data = getData(DATA_PIN)
-            tmp_temperature = tonumber(dht11_data.temperature)
-            tmp_humidity = tonumber(dht11_data.humidity)
-            if(temperature ~= tmp_temperature or humidity ~= tmp_humidity) then              
-                DATA = '{"mac":"'..wifi.sta.getmac()..'", "ip":"'..wifi.sta.getip()..'",'
-                DATA = DATA..'"temperature":"'..tmp_temperature..'","humidity":"'..tmp_humidity..'"}'
-                -- Publish a message (QoS = 0, retain = 0)
-                m:publish(TOPIC, DATA, 0, 0, function(conn)
-                    print(CLIENT_ID.." sending data: "..DATA.." to "..TOPIC)
-                end)
-            else
-                print("No change in value, no data send to broker.")
-            end
-        end)
     end)
 end)
-
-m:close();
