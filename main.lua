1c01efe492f6ed426218db63fdaa004c1c4c221c
bot <contact@arca-computing.fr>
2016-06-02 08:30:09 +0200

    Add last will to mqtt

--- main.lua
+++ main.lua
@@ -6,6 +6,10 @@ TOPIC = "/sensors/"..LOCATION.."/dht11/data"
 -- Init client with keepalive timer 120sec
 m = mqtt.Client(CLIENT_ID, 120, "", "")
 
+ip = wifi.sta.getip()
+
+m:lwt("/offline", '{"message":"'..CLIENT_ID..'", "topic":"'..TOPIC..'", "ip":"'..ip..'"}', 0, 0)
+
 print("Connecting to MQTT: "..BROKER_IP..":"..BROKER_PORT.."...")
 m:connect(BROKER_IP, BROKER_PORT, 0, 1, function(conn)
     print("Connected to MQTT: "..BROKER_IP..":"..BROKER_PORT.." as "..CLIENT_ID)
@@ -13,7 +17,7 @@ m:connect(BROKER_IP, BROKER_PORT, 0, 1, function(conn)
         dht11_data = getData(DATA_PIN)
         temperature = tonumber(dht11_data.temperature)
         humidity = tonumber(dht11_data.humidity)        
-        DATA = '{"mac":"'..wifi.sta.getmac()..'", "ip":"'..wifi.sta.getip()..'",'
+        DATA = '{"mac":"'..wifi.sta.getmac()..'", "ip":"'..ip..'",'
         DATA = DATA..'"temperature":"'..temperature..'","humidity":"'..humidity..'"}'        
         -- Publish a message (QoS = 0, retain = 0)
         m:publish(TOPIC, DATA, 0, 0, function(conn)
